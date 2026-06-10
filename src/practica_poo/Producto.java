6f51b26d80bf7a475a991853a201b111eddf436b
alex <ch690658@gmail.com>
2026-06-10 14:23:50 -0600

    clase producto

--- src/practica_poo/Producto.java
+++ src/practica_poo/Producto.java
@@ -1,5 +1,31 @@
 package practica_poo;
 
 public class Producto {
+	 private String nombre;
+	    private double precio;
+	    private String codigo;
+
+	    public Producto(String nombre, double precio, String codigo) {
+	        this.nombre = nombre;
+	        this.precio = precio;
+	        this.codigo = codigo;
+	    }
+
+	    public String getNombre() { return nombre; }
+	    public void setNombre(String nombre) { this.nombre = nombre; }
+
+	    public double getPrecio() { return precio; }
+	    public void setPrecio(double precio) { this.precio = precio; }
+
+	    public String getCodigo() { return codigo; }
+	    public void setCodigo(String codigo) { this.codigo = codigo; }
+
+	    public void mostrarInfo() {
+	        System.out.println("=== PRODUCTO ===");
+	        System.out.println("Código: " + codigo);
+	        System.out.println("Nombre: " + nombre);
+	        System.out.println("Precio: $" + precio);
+	        //hola
+	    }
 
 }
